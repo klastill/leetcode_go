58ad3504fbc42567f73a86fc8ca83a0636227f1a
binmariney <binmariney@gmail.com>
2022-02-08 11:33:35 +0900

    fix commit

--- problem_004.go
+++ problem_004.go
@@ -1,17 +1,30 @@
 package leetcode
 
-func twoSum(nums []int, target int) []int {
-	var answer []int
-	for i, first := range nums {
-		for j, second := range nums[i+1:] {
-			if first+second == target {
-				answer = append(answer, i, i+j+1)
-			}
-		}
+import (
+	"fmt"
+	"sort"
+)
+
+func findMedianSortedArrays(nums1 []int, nums2 []int) float64 {
+	var answer float64
+	var merged []int
+	merged = append(merged, nums1...)
+	merged = append(merged, nums2...)
+	sort.Slice(merged, func(i, j int) bool {
+		return merged[i] < merged[j]
+	})
+	fmt.Println(merged)
+	if len(merged)%2 == 0 {
+		answer = float64(merged[len(merged)/2-1]+merged[len(merged)/2]) / float64(2)
+	} else {
+		answer = float64(merged[len(merged)/2])
 	}
 	return answer
 }
 
 //test case:
-//[2,7,11,15] 9
-// [3,2,4] 6
+//[1,3] [4]
+//[1,2,3] [4]
+
+// Runtime: 24 ms, faster than 37.00% of Go online submissions for Median of Two Sorted Arrays.
+// Memory Usage: 6.4 MB, less than 28.06% of Go online submissions for Median of Two Sorted Arrays.
